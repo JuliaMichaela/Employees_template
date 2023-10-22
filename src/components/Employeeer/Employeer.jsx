56564b0ec2f28b02f4734959640cb208175e0edb
JuliaMichaela <juliaperez@yandex.ru>
2023-10-22 22:55:09 +0300

    added Employeer.jsx

---
+++ src/components/Employeeer/Employeer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+export const Employeer = (props) => {
+    return (
+        <div>Employeer</div>
+    )
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Employeer)
